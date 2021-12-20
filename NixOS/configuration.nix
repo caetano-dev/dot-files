c8fe775a51bea1049545903f364e7cfa4d164db3
Drull <pcaetanop@protonmail.com>
2021-12-20 20:57:43 -0300

    Add files via upload

--- NixOS/configuration.nix
+++ NixOS/configuration.nix
@@ -64,12 +64,9 @@
 
 	  windowManager.i3 = {
 		  enable = true;
+		  package = pkgs.i3-gaps;
 		  extraPackages = with pkgs; [
-        dmenu 
 	i3status 
-        i3lock 
-        i3block
-	i3-gaps
         rofi
      ];
     };
